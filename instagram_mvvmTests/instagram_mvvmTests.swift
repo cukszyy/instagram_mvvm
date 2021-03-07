b3d5fa7716987bc67256d87a8f3156054fd672ad
Lucas Portella <lucas.portella>
2021-03-07 16:19:57 -0300

    Initial Commit

---
+++ instagram_mvvmTests/instagram_mvvmTests.swift
@@ -0,0 +1,33 @@
+//
+//  instagram_mvvmTests.swift
+//  instagram_mvvmTests
+//
+//  Created by Lucas Portella on 07/03/21.
+//
+
+import XCTest
+@testable import instagram_mvvm
+
+class instagram_mvvmTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
